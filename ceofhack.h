abec5bab57e30ad9dd6c63cce8a2b6f8c1f22977
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-06-27 09:21:45 +0200

    add header
    Signed-off-by: Nico Schottelius <nico@denkbrett.schottelius.org>

---
+++ ceofhack.h
@@ -0,0 +1,14 @@
+#ifndef THIS_IS_AN_UGLY_CEOF_HACK
+#define THIS_IS_AN_UGLY_CEOF_HACK
+
+/* the library version is not yet used */
+
+#define LIBEOF_VERSION "E.O.F"
+
+/* those values are from the current EOF specs */
+
+#define EOF_L_NICKNAME 128
+#define EOF_L_MESSAGE  128
+
+
+#endif
