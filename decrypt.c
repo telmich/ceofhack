86f3d41a53834231eea54da750bd4564c9c13ae0
Nico Schottelius <nico@tr.schottelius.org>
2008-07-06 21:30:47 +0200

    testcase with working/non-working version
    Signed-off-by: Nico Schottelius <nico@tr.schottelius.org>

--- decrypt.c
+++ decrypt.c
@@ -23,11 +23,13 @@ int main()
    gpgme_ctx_t g_context;
    gpgme_error_t gerr;
    gpgme_data_t g_plain_recv;
+   gpgme_data_t g_plain_recv2;
    gpgme_data_t g_encrypt_send;
+   gpgme_data_t g_test;
+
 
    char *p;
    char b_encrypt[BIGBUF+1];
-
    int i, tmp;
    int fd;
 
@@ -68,23 +70,39 @@ int main()
    /* create buffers */
    gerr = gpgme_data_new(&g_plain_recv);
    if(gerr != GPG_ERR_NO_ERROR) return 20;
-   /* FIXME: test: it is not in t-decrypt.c */
+
+   gerr = gpgme_data_new(&g_plain_recv2);
+   if(gerr != GPG_ERR_NO_ERROR) return 25;
+
+   gerr = gpgme_data_new(&g_test);
+   if(gerr != GPG_ERR_NO_ERROR) return 20;
+
    i = strlen(b_encrypt);  
+   printf("strlen(%s) = %d\n",b_encrypt,i);
+
+   /* THIS WORKS */
    gerr = gpgme_data_new_from_mem(&g_encrypt_send, b_encrypt, i, 1);
-   //gerr = gpgme_data_new(&g_encrypt_send);
-//   gerr = gpgme_data_new_from_file (&g_encrypt_send, "./testcrypt", 1);
    if(gerr != GPG_ERR_NO_ERROR) return 24;
    
-/* 
-   printf("strlen(%s) = %d\n",b_encrypt,i);
-   gpgme_data_write(g_encrypt_send, b_encrypt, i); */
+   /* THIS DOES NOT WORK */
+   tmp = gpgme_data_write(g_test, b_encrypt, i);
+   printf("gpgmedatawrite: %d\n", tmp);
 
-   /* decrypt */
+   /* decrypt: from gpgme_data_new_from_mem()  */
    gerr = gpgme_op_decrypt(g_context, g_encrypt_send, g_plain_recv);
    if(gerr != GPG_ERR_NO_ERROR) {
       printf("gerr=%d\n",gerr);
       return 19;
    }
 
+   /* decrypt: from gpgme_data_write() */
+   gerr = gpgme_op_decrypt(g_context, g_test, g_plain_recv2);
+   if(gerr != GPG_ERR_NO_ERROR) {
+      printf("gerr=%d\n",gerr);
+      return 41;
+   }
+
    return 1;
 }
+//   this works
+//   gerr = gpgme_data_new_from_file (&g_encrypt_send, "./testcrypt", 1);
