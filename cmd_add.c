862ca9ecd6bd29af90ff83c4cd795d3aa6dd97fc
Nico Schottelius <nico@tr.schottelius.org>
2008-08-18 17:08:39 +0200

    finisih cmd_add, inclusive debugging
    Signed-off-by: Nico Schottelius <nico@tr.schottelius.org>

---
+++ cmd_add.c
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Init commands and handler
+ *
+ */
+
+#include <unistd.h>
+
+#include "ceofhack.h"  /* functions etc. */
+
+int cmd_add(char *name, int (*handle)(char *))
+{
+   struct cmd *new;
+
+   new = malloc(sizeof(struct cmd));
+   if(!new) return 0;
+
+   new->name   = name;
+   new->handle = handle;
+   new->next   = cmds.next;
+
+   cmds.next   = new;
+
+   //for(cur = cmds; cur->next != NULL; cur = cur->next) ;
+   //cur->next = new;
+   //cmds_last->next = new;
+   //cmds_last = new;
+   
+   return 1;
+}
