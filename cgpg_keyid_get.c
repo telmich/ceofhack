58f77c62ba69538d28b21bcf558d92d3241c384b
Nico Schottelius <nico@ikn.schottelius.org>
2009-03-16 19:50:00 +0100

    add functions to retrieve keyid
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ cgpg_keyid_get.c
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Initialise the GPGME part
+ *
+ */
+
+#include <stdio.h>      /* printf            */
+#include <gpgme.h>      /* gpgme             */
+#include "ceofhack.h"   
+
+int cgpg_keyid_get(char *nick, gpgme_key_t keyid[])
+{
+ //  gpgme_error_t gerr;
+
+   /* retrieve keyid */
+
+//   gpgme_op_keylist_start(gpg_context, 
+   
+   return 1;
+}
---
+++ peer_keyid_get.c
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Return keyid
+ *
+ */
+
+#include <string.h>    /* memset, str*    */
+#include <stdio.h>     /* NULL            */
+#include "ceofhack.h"  /* functions etc.  */
+
+char *peer_keyid_get(char *nick)
+{
+   struct peers *p;
+
+   for(p = plist.next; p != NULL; p = p->next) {
+      if(!strcmp(nick, p->peer.name)) {
+         printf("Keyid (%s) for peer %s found!\n", p->peer.keyid, p->peer.name);
+         return p->peer.keyid;
+      }
+   }
+
+   return NULL;
+}
