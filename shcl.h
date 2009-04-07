77db252c77b8322b160817fb0da9a938fb439e66
Nico Schottelius <nico@ikn.schottelius.org>
2009-04-07 20:53:20 +0200

    add broken shcl.h
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ shcl.h
@@ -0,0 +1,11 @@
+#ifndef SHCL_H
+#define SHCL_H
+
+/* header for shcl: should be merged back later */
+static void reverse(char* s);
+static void convert(unsigned long value, unsigned int base, char* converted, size_t size);
+static int boundary(unsigned int base, size_t size, size_t typesize);
+int ultostr(unsigned long value, unsigned int base, char* converted, size_t size);
+int ltostr(long value, unsigned int base, char* converted, size_t size);
+
+#endif
