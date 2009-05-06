53e2c2049c8494d23ea3cad02425c6f35caa0a37
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-06 10:54:42 +0200

    Ditz-issue: 55878fce73525180a6dc4f242570578d9ae98fe4

---
+++ read_all.c
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ *
+ * 2009     Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Read all incoming data
+ *
+ */
+
+#include <unistd.h>     /* write          */
+#include <errno.h>      /* errno          */
+#include <stdio.h>      /* perror         */
+
+ssize_t read_all(int fd, void *buf, size_t count)
+{
+   ssize_t tmp;
+
+   /* loop until we operate without being interrupted */
+   while((size_t) tmp < count) {
+      tmp = read(fd, buf, count);
+      if(tmp == -1) {
+         if(errno == EINTR) { /* retry */
+            continue;
+         } else {
+            perror("read_all");
+            return -1;
+         }
+      }
+      break;
+   }
+
+   return tmp;
+}
+
