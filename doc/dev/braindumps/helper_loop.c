2fe5496e8d1e2ded97b44a273af0891eee4a7eae
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-08 23:28:26 +0200

    add some old braindumps
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ doc/dev/braindumps/helper_loop.c
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Loop through all helpers
+ *
+ */
+
+#include "ceofhack.h"  /* functions etc. */
+
+/* save offset && count!!!!!! */
+
+int helper_loop(int *offset)
+{
+   int i;
+
+   for(i=0; i < MAX_COMM; i++) {
+      /* no handler? free for use */
+      if(!chp[i].handle) {
+         ++chp_cnt;
+         return i;
+      }
+   }
+
+   /* this should never happen and means there's a bug in the helper system  */
+   return -2;
+}
