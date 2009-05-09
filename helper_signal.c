c5faa02f99648e79a8ba7042799af69ae2006d82
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-09 18:31:40 +0200

    add templates for helper_signal_*
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ helper_signal.c
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Leave ceofhack
+ * - Shutdown helper: transport protocols, user interfaces
+ * - [Send exit command to all and] send SIGINT
+ *
+ */
+
+#include <signal.h>     /* kill()         */
+#include <unistd.h>     /* _exit()        */
+#include "ceofhack.h"
+
+void ceof_exit(int i)
+{
+   printf("Shutting down EOF subsystems...\n");
+
+   /* cycle through all helper, notify them */
+   for(i=0; i < MAX_COMM; i++) {
+      if(chp[i].pid) {
+         if(kill(chp[i].pid, SIGINT) == -1) {
+            perror("kill");
+         }
+      }
+   }
+   _exit(0);
+}
