482ace221dc585ff4af0ecfac0681c0c6a887cff
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-09 10:37:47 +0200

    remove broken debug from tp_send.c
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- tp_send.c
+++ tp_send.c
@@ -77,7 +77,7 @@ int tp_send(char *nick, char *msg)
 
    len = strlen(msg);
    tmp = ultostr(len, 10, p, BIGBUF - (p-buf));
-   printf("ultostr: %d, (%lu)\n", tmp, len);
+//   printf("ultostr: %d, (%d)\n", tmp, len);
    len = EOF_L_CMD + EOF_L_ADDRESS + strlen(p) + 1 + strlen(msg);
    strncat(p, "\n", BIGBUF);
    strncat(p, msg, BIGBUF);
