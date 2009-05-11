c096f6890c60a330c88e42e8d11eb2b36d7823fd
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-11 18:33:27 +0200

    some cleanups for gen_key.sh
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- eof.h
+++ eof.h
@@ -1,13 +1,14 @@
 #ifndef EOF_H
 #define EOF_H
 
-/* lengths */
+/* Lengths */
 #define EOF_L_CMD       4
+#define EOF_L_KEYID     40
 #define EOF_L_NICKNAME  128
 #define EOF_L_MESSAGE   128
 #define EOF_L_ADDRESS   128
 #define EOF_L_CHANNEL   128
-#define EOF_L_KEYID     40
+#define EOF_L_UI_INPUT  256
 #define EOF_L_PKG_MAX   65536
 
 /* areas / categories / components */
--- scripts/gen_key.sh
+++ scripts/gen_key.sh
@@ -1,18 +1,39 @@
 #!/bin/sh
+# 
+# 2009      Nico Schottelius (nico-ceofhack at schottelius.org)
+# 
+# This file is part of ceofhack.
+#
+# ceofhack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ceofhack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ceofhack. If not, see <http://www.gnu.org/licenses/>.
+#
 
 gpgdir="$HOME/.ceof/gpg"
 
-if [ -e "$gpgdir/secring.sec" ]; then
-	exit 0
-fi
+# gpg wants a secure directory
+umask 077
+
+[ -e "$gpgdir/secring.gpg" ] && exit 0
 
 if [ ! -x "`which gpg`" ]; then
 	echo "gpg not found" 1>&2
 	exit 1
 fi
 
-mkdir -p "$gpgdir" || exit 1
-cd "$gpgdir" || exit 1
+set -e
+
+mkdir -p "$gpgdir"
+cd "$gpgdir"
 
 cat > keygen.batch << EOF
 	%echo Generating a standard key
@@ -21,13 +42,12 @@ cat > keygen.batch << EOF
 	Subkey-Type: ELG-E
 	Subkey-Length: 1024
 	Name-Real: $USER
-	Name-Comment: EOF
+	Name-Comment: EOF-2
 	Expire-Date: 0
 	%commit
 	%echo done
 EOF
 
-gpg --homedir "$gpgdir" --batch --gen-key keygen.batch || exit 1
+gpg --homedir "$gpgdir" --batch --gen-key keygen.batch
 
 exit 0
-
