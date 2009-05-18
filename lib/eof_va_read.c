f1d9f2339779b6a85d16b091ef1ac569710eb1ee
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-18 18:50:37 +0200

    add eof_va_read.c and eof_va_write.c
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ lib/eof_va_read.c
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ *
+ * 2009      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Read a variable number of arguments, which are passed in pairs:
+ * In case of nof = 2:
+ * 
+ *    eof_va_read(1, 2, len1, buf1, len2, buf2);
+ *
+ */
+
+#include <stdarg.h>  /* va_*)             */
+
+#include "shcl.h"    /* functions etc.    */
+#include "eof.h"     /* defines           */
+
+int eof_va_read(int sockfd, int nof, ...) /* nof = number of fields */
+{
+   va_list ap;
+   int len;
+   char *data;
+
+   va_start(ap, nof);
+   
+   while(nof > 0) {
+      len =    va_arg(ap, int);
+      data =   va_arg(ap, char *);
+
+      if(read_all(sockfd, data, len) != len) {
+         return -1;
+      }
+
+      --nof;
+   }
+
+   va_end(ap);
+
+   return 1;
+}
