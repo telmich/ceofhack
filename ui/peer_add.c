3bc456da65ad641ca581976084c43fe359868e71
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-20 08:51:39 +0200

    copy peer_add to ui
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ ui/peer_add.c
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Add a peer
+ *
+ */
+
+#include <string.h>     /* memset, str*   */
+#include <stdlib.h>     /* calloc         */
+#include <stdio.h>     /* printf         */
+
+#include "ceofhack.h"   /* functions etc.  */
+#include "ui.h"         /* functions etc.  */
+
+int peer_add(char *str)
+{
+   struct peers *p;
+
+   p = calloc(1, sizeof(struct peers));
+   if(!p) return 0;
+
+   /* <empty> name <empty> addr <empty> keyid */
+
+   if(!ui_cmd_argcnt(str, 3)) {
+      printf("Usage: /peer add <name> <addr> <keyid>\n");
+      return 0;
+   }
+
+   /* nick */
+   str = ui_cmd_argncpy(str, p->peer.name, EOF_L_NICKNAME);
+   str = ui_cmd_argncpy(str, p->peer.addr, EOF_L_ADDRESS);
+   str = ui_cmd_argncpy(str, p->peer.keyid, EOF_L_KEYID);
+
+   p->next = plist.next;
+   plist.next = p;
+
+   printf("/peer add: Added <%s>@<%s>:<%s>\n",p->peer.name, p->peer.addr, p->peer.keyid);
+
+   return 1;
+}
