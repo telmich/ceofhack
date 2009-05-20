3be5d0c6470bb8d2e10bfb9740d3f2eb96c89ea5
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-20 12:16:50 +0200

    -typo
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- peer_init.c
+++ peer_init.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * 2008      Nico Schottelius (nico-ceofhack at schottelius.org)
+ * 2009      Nico Schottelius (nico-ceofhack at schottelius.org)
  *
  * This file is part of ceofhack.
 
@@ -22,14 +22,14 @@
  *
  */
 
-#include <stdio.h>      /* NULL */
-#include "ceofhack.h"   /* functions etc. */
+#include <stdio.h>      /* NULL              */
+#include "ceofhack.h"   /* functions etc.    */
 
-struct peers plist;
+struct peer plist;
 
-int peers_init()
+int peer_init()
 {
-   plist.next = NULL;
+   plist.next = NULL; /* explicit */
 
    return 1;
 }
