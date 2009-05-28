2dec8c5f034f5bbf6031c28098a0506cb0af15fc
Nico Schottelius <nico@ikn.schottelius.org>
2009-05-28 19:22:11 +0200

    add template for cmd_2199
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ cmd_2199.c
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *
+ * 2009      Nico Schottelius (nico-ceofhack at schottelius.org)
+ *
+ * This file is part of ceofhack.
+
+ * ceofhack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ceofhack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ceofhack.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * Handle /peer show request
+ *
+ */
+
+#include <string.h>     /* memset()          */
+#include <stdio.h>      /* printf()          */
+#include "ceofhack.h"   /* functions etc.    */
+
+int cmd_2105(int fd[])
+{
+   printf("UI: /peer show request\n");
+   
+   return peer_show(fd);
+}
