0701711be0d6265a5e5d0d598278d0c03f4f5c09
Nico Schottelius <nico@kr.ethz.ch>
2010-06-03 19:40:58 +0200

    add unit tests with check
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

---
+++ tests.c
@@ -0,0 +1,44 @@
+#include <check.h>
+#include <stdio.h>
+
+#include <eof.h>
+
+unsigned long eof_id = 42;
+
+START_TEST (t_eof_id_new)
+{
+   char buf[EOF_L_ID];
+   int a, b;
+   a = 2; b = 3;
+
+   eof_id_new(buf);
+   printf("id: %s\n", buf);
+
+   fail_unless ((a) == (b), "Not equal");
+}
+END_TEST
+
+Suite *queue_suite (void)
+{
+  Suite *s = suite_create ("queue");
+
+  TCase *tc_core = tcase_create ("Core");
+  tcase_add_test (tc_core, t_eof_id_new);
+  suite_add_tcase (s, tc_core);
+
+  return s;
+}
+
+int main (void)
+{
+   int number_failed;
+
+   Suite *s = queue_suite ();
+   SRunner *sr = srunner_create (s);
+
+   srunner_run_all (sr, CK_NORMAL);
+   number_failed = srunner_ntests_failed (sr);
+   srunner_free (sr);
+   
+   return 0;
+}
