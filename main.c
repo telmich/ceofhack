6753183d1c6a1cae4e671465ae41778acc711e51
Nico Schottelius <nico@kr.ethz.ch>
2010-06-10 19:45:28 +0200

    disable eofs for now, until structures are setup
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ int main()
    }
 
    if(!eof_id_init())   return CEOF_EX_ID;         /* init id generator    */
-   if(!eofs_init())     return CEOF_EX_EOFS;       /* init helper structs  */
+//   if(!eofs_init())     return CEOF_EX_EOFS;       /* init helper structs  */
    if(!helper_init())   return CEOF_EX_HELPER;     /* init helper structs  */
    if(!cgpg_init())     return CEOF_EX_GPG;        /* init gpgme           */
    if(!signals_init())  return CEOF_EX_SIGNALS;    /* catch signals        */
