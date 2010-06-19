7406f61bf22fbcedce962f578841fe37b647e14e
Nico Schottelius <nico@kr.ethz.ch>
2010-06-19 16:20:05 +0200

    beautify eof.h
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- eof.h
+++ eof.h
@@ -27,58 +27,60 @@
 #define EOF_H
 
 /* Lengths */
-#define EOF_L_CMD                4
-#define EOF_L_SIZE               6
-#define EOF_L_ID                 6
-#define EOF_L_KEYID              40
-#define EOF_L_NICKNAME           128
-#define EOF_L_MESSAGE            128
-#define EOF_L_ADDRESS            128
-#define EOF_L_GROUP              128
-#define EOF_L_UI_INPUT           256
-#define EOF_L_PKG_ONION          EOF_L_CMD+EOF_L_ID+EOF_L_ADDRESS+EOF_L_GROUP+EOF_L_MESSAGE
-#define EOF_L_PKG_MAX            65536
+#define EOF_L_CMD                   4
+#define EOF_L_SIZE                  6
+#define EOF_L_ID                    6
+#define EOF_L_KEYID                 40
+#define EOF_L_NICKNAME              128
+#define EOF_L_MESSAGE               128
+#define EOF_L_ADDRESS               128
+#define EOF_L_GROUP                 128
+#define EOF_L_UI_INPUT              256
+#define EOF_L_PKG_ONION             EOF_L_CMD+EOF_L_ID+EOF_L_ADDRESS+EOF_L_GROUP+EOF_L_MESSAGE
+#define EOF_L_PKG_MAX               65536
 
 /* commands */
-#define EOF_CMD_TPS              "1000"
-#define EOF_CMD_TPL_START        "1001"
-#define EOF_CMD_TPL_STOP         "1002"
+#define EOF_CMD_TPS                 "1000"
+#define EOF_CMD_TPL_START           "1001"
+#define EOF_CMD_TPL_STOP            "1002"
 
-#define EOF_CMD_TPS_DEFAULT      "20xx"
-#define EOF_CMD_TPS_SENT         "2000"
-#define EOF_CMD_TPS_NOT_SENT     "2001"
-#define EOF_CMD_TPL_RECV         "2002"
-#define EOF_CMD_TPL_LISTENING    "2003"
+#define EOF_CMD_TPS_DEFAULT         "20xx"
+#define EOF_CMD_TPS_SENT            "2000"
+#define EOF_CMD_TPS_NOT_SENT        "2001"
+#define EOF_CMD_TPL_RECV            "2002"
+#define EOF_CMD_TPL_LISTENING       "2003"
 
 /* queue/id */
-#define EOF_QUEUE_ID_CHARS       "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-!"
+#define EOF_QUEUE_ID_CHARS          "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-!"
 
 /* user interfaces: already implemented */
-#define EOF_CMD_UI_ACK           "1100"
-#define EOF_CMD_UI_FAIL          "1101"
-#define EOF_CMD_UI_EXITREQUEST   "1102"
-#define EOF_CMD_UI_MSGRECEIVED   "1103"
-#define EOF_CMD_UI_PEER_LISTING  "1104"
-#define EOF_CMD_UI_PEER_INFO     "1105"
-#define EOF_CMD_UI_PEER_RENAMED  "1106"
+#define EOF_CMD_UI_ACK              "1100"
+#define EOF_CMD_UI_FAIL             "1101"
+#define EOF_CMD_UI_EXITREQUEST      "1102"
+#define EOF_CMD_UI_MSGRECEIVED      "1103"
+#define EOF_CMD_UI_PEER_LISTING     "1104"
+#define EOF_CMD_UI_PEER_INFO        "1105"
+#define EOF_CMD_UI_PEER_RENAMED     "1106"
 
-#define EOF_CMD_UI_DEFAULT       "21xx"
-#define EOF_CMD_UI_REGISTER      "2100"
-#define EOF_CMD_UI_DEREGISTER    "2101"
-#define EOF_CMD_UI_PEER_ADD      "2102"
-#define EOF_CMD_UI_PEER_SEND     "2103"
-#define EOF_CMD_UI_PEER_RENAME   "2104"
-#define EOF_CMD_UI_PEER_SHOW     "2105"
-#define EOF_CMD_UI_PEER_LIST     "2106"
-#define EOF_CMD_UI_QUIT          "2199"
+#define EOF_CMD_UI_DEFAULT          "21xx"
+#define EOF_CMD_UI_REGISTER         "2100"
+#define EOF_CMD_UI_DEREGISTER       "2101"
+#define EOF_CMD_UI_PEER_ADD         "2102"
+#define EOF_CMD_UI_PEER_SEND        "2103"
+#define EOF_CMD_UI_PEER_RENAME      "2104"
+#define EOF_CMD_UI_PEER_SHOW        "2105"
+#define EOF_CMD_UI_PEER_LIST        "2106"
+#define EOF_CMD_UI_QUIT             "2199"
 
+/* crypto engine */
+#define EOF_CMD_CRYPTO_ENCRYPT      "2199"
 
 /* decoded packets from outside */
-#define EOF_CMD_ONION_DROP         "3000"
-#define EOF_CMD_ONION_FORWARD      "3001"
-#define EOF_CMD_ONION_MSG_DROP     "3002"
-#define EOF_CMD_ONION_MSG_FORWARD  "3003"
-#define EOF_CMD_ONION_ACK          "3004"
+#define EOF_CMD_ONION_DROP          "3000"
+#define EOF_CMD_ONION_FORWARD       "3001"
+#define EOF_CMD_ONION_MSG_DROP      "3002"
+#define EOF_CMD_ONION_MSG_FORWARD   "3003"
+#define EOF_CMD_ONION_ACK           "3004"
 
 /* UI commands */
 #define EOF_UI_EXIT              "/exit"
